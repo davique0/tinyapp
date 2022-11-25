d70de8c69d66e48a4565e780a73b8189da411bed
davique0 <davique@gmail.com>
2022-11-24 20:31:17 -0500

    set express_server server, with get response for root path

---
+++ express_server.js
@@ -0,0 +1,18 @@
+const express = require("express"); //add express library
+const app = express(); //define our app as an instance of express
+const PORT = 8080; //defines port at 8080
+
+const urlDatabase = {
+  'bZxVnZ': 'http://www.lighthouse.ca',
+  '9sm5xK': 'http://www.google.com'
+};
+
+//set server request for root path
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+//set server with port and sends message with port number
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
