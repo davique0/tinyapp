5c8aef0a9a01a5b02314c178f611654bb01ee3d0
davique0 <davique@gmail.com>
2022-11-24 23:23:33 -0500

    add additional route for longURL id or short URL

--- express_server.js
+++ express_server.js
@@ -20,6 +20,12 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 })
 
+//add a new route for :id
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] }
+  res.render('urls_show', templateVars);
+});
+
 //add routes from urlDarabase object
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase)
