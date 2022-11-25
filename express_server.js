6f88ddf2bdc874be78a276b9ab9192f599a768ce
davique0 <davique@gmail.com>
2022-11-25 11:28:12 -0500

    add POST request to express_server for longURL entered by user

--- express_server.js
+++ express_server.js
@@ -2,7 +2,8 @@ const express = require('express'); //add express library
 const app = express(); //define our app as an instance of express
 const PORT = 8080; //defines port at 8080
 
-app.set('view engine', 'ejs') //set EJS as our template engine
+app.set('view engine', 'ejs'); //set EJS as our template engine
+app.use(express.urlencoded({ extended: true })); //Make buffer readable
 
 const urlDatabase = {
   'bZxVnZ': 'http://www.lighthouse.ca',
@@ -16,24 +17,29 @@ app.get("/", (req, res) => {
 
 //add route for /urls
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase }
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
-})
+});
 
 //add a new route to 'new' page
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body); //log the POST request body to the console
+  res.send('Ok'); // Respond with 'Ok'
+});
+
 //add a new route for any ID that goes after urls and doesnt exist yet
 app.get('/urls/:id', (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] }
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
 
 //add routes from urlDarabase object
 app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase)
+  res.json(urlDatabase);
 });
 
 //send HTML as response
