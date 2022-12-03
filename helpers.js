ade5f08a48d0b134f401126987559fec373bf268
davique0 <davique@gmail.com>
2022-12-03 12:04:08 -0500

    create new file for helper functions

---
+++ helpers.js
@@ -0,0 +1,13 @@
+
+
+//Check if email and password arent empty and if they are already in the database
+const getUserByEmail = (email, database) => {
+  for (const userId in database) {
+    if (database[userId].email === email) {
+      return database[userId];
+    }
+  }
+  return null;
+};
+
+module.exports = { getUserByEmail }
